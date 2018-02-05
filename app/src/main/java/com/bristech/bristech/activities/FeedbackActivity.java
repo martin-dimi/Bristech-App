f914b055f76af6e5febfb29af6e491a2fd125d68
Eli <ek16138@my.bristol.ac.uk>
2018-02-05 10:33:09 +0000

    Added some buttons within Settings and Event detail activities

---
+++ app/src/main/java/com/bristech/bristech/activities/FeedbackActivity.java
@@ -0,0 +1,15 @@
+package com.bristech.bristech.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bristech.bristech.R;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
